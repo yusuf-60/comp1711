da95aa169d4d7bf2f25d58296df04bcddaee9748
yusuf-60 <146723539+yusuf-60@users.noreply.github.com>
2023-10-12 14:31:17 +0000

    session 2

---
+++ week2/session2/intro_task.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int num1;
+    int num2;
+    int total;
+
+    printf("enter your first number: ");
+    scanf("%d",&num1);
+    printf("enter the second number: ");
+    scanf("%d",&num2);
+
+    total=num1+num2;
+    printf("your total is %d \n",total);
+    return 0;    
+}
+
+
