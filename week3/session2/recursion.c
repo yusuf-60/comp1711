bec9dfe9a62f906b36184ccd6d69654fd31b2f24
yusuf-60 <146723539+yusuf-60@users.noreply.github.com>
2023-10-19 15:48:12 +0000

    functions and recursion

--- week3/session2/recursion.c
+++ week3/session2/recursion.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 
-int recursion_1(double int(n)){
+int recursion_1(int(n)){
     if (n>1){
         n*=recursion_1(n-1);
     }
     return n;
 }
 int main(){
-    double int n;
+    int n;
 
     printf("enter a value to return the factorial of: \n");
     scanf("%d",&n);
 
-    printf("this is the value returned %d\n",recursion_1(n));
+    printf("this is the value returned %d",recursion_1(n));
     return 0;
 }
