c8bcaba96fd5b20a4a0f0aee0fc25fd50f401874
yusuf-60 <146723539+yusuf-60@users.noreply.github.com>
2023-12-09 16:12:20 +0000

    comment

--- task2/StepCounter.c
+++ task2/StepCounter.c
@@ -8,10 +8,10 @@
 
 // Define any additional variables here
 // Global variables for filename and FITNESS_DATA array
-char temp_date[11];
-char temp_time[60];
-char temp_date2[11];
-char temp_time2[60];
+char lowest_date[11];
+char lowest_time[60];
+char highest_date[11];
+char highest_time[60];
 
 char start_time[40];
 char end_time[40];
@@ -54,6 +54,8 @@ int read_info(const char *file_name,int *counter, int *mean_steps){
     char time[60];       
     char steps[10];
 
+    *counter = 0;
+    *mean_steps = 0;
     int total_steps = 0;
     float unrounded_mean = 0;
 
@@ -64,16 +66,13 @@ int read_info(const char *file_name,int *counter, int *mean_steps){
     int difference_index = 0;
     int start_index;
 
+
     FILE *file = fopen(file_name , "r");
     if (file == NULL){
-        fprintf(stderr,"Error: Could not find or open the file.\n");
         return 1;
-        exit(-1);
     }else{
         printf("File successfully loaded.\n");
-    }
-    *counter = 0;
-    *mean_steps = 0;
+    
     while (fgets( line, buffer , file)){
 
     tokeniseRecord(line,",",date,time,steps);
@@ -81,24 +80,24 @@ int read_info(const char *file_name,int *counter, int *mean_steps){
     strcpy(fitdata[*counter].date,date);
     strcpy(fitdata[*counter].time,time);
     fitdata[*counter].steps = atoi(steps);
-    (*counter)++;
+    (*counter) =*counter+1;
     }
 
     lowest_steps=0;
     highest_steps=0;
     for (count=0;count<*counter;count++){
         total_steps += fitdata[count].steps;
-        strcpy(temp_date,(fitdata[count].date));
-        strcpy(temp_time,(fitdata[count].time));
+        strcpy(lowest_date,(fitdata[count].date));
+        strcpy(lowest_time,(fitdata[count].time));
         if (fitdata[count].steps<lowest_steps){
             lowest_steps =fitdata[count].steps;
             //  strcpy(tdetail.date,(fitdata[count].date));
-            strcpy(temp_date,(fitdata[count].date));
-            strcpy(temp_time,(fitdata[count].time));
+            strcpy(lowest_date,(fitdata[count].date));
+            strcpy(lowest_time,(fitdata[count].time));
         }else if(fitdata[count].steps>highest_steps){
             highest_steps = fitdata[count].steps;
-            strcpy(temp_date2,(fitdata[count].date));
-            strcpy(temp_time2,fitdata[count].time);
+            strcpy(highest_date,(fitdata[count].date));
+            strcpy(highest_time,fitdata[count].time);
         }
     }
     
@@ -129,11 +128,11 @@ int read_info(const char *file_name,int *counter, int *mean_steps){
     strcat(start_time,(fitdata[difference_index-highest_streak].time));
 
     
-    unrounded_mean = total_steps/(*counter);
+    unrounded_mean = (float)total_steps/(*counter);
     *mean_steps = round(unrounded_mean);
-    printf("%d\n",*counter);
     fclose(file);
     return 0;
+}
 }
 
 // Complete the main function
@@ -141,22 +140,25 @@ int main() {
     char file_name[100];
    
     char option;
-    int counter = 0 ;
+    int counter = 15 ;
     int mean_steps = 0;
     while(1){
         printf("Menu Options:\nA: Specify filename to be imported\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the date and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Quit\nEnter choice: ");
-        scanf("%s", &option);
+        scanf(" %c", &option);
         switch (option){
                 case 'A':printf("Input filename: ");scanf("%s",file_name);
-                read_info(file_name,&counter,&mean_steps);
+                if (read_info(file_name, &counter, &mean_steps) != 0){
+                    fprintf(stderr,"Error: Could not find or open the file.\n");
+                    exit(1);
+                }
                 break;
-                case 'B':printf("Total records: %d\n", counter);
+                case 'B':printf("Total records: %d\n",counter);
                 break;
                 case 'C':
-                printf("Fewest steps: %s %s\n",temp_date,temp_time);
+                printf("Fewest steps: %s %s\n",lowest_date,lowest_time);
                 break;
                 case 'D':
-                printf("Largest steps: %s %s\n",temp_date2,temp_time2);
+                printf("Largest steps: %s %s\n",highest_date,highest_time);
                 break;
                 case 'E':   
                 printf("Mean step count: %d\n",mean_steps);
@@ -165,7 +167,7 @@ int main() {
                 printf("Longest period start: %s \n",start_time);
                 printf("Longest period end: %s\n", end_time);
                 break;
-                case 'Q':   return 0;
+                case 'Q':
                 exit(-1);
                 default: printf("Invalid choice. Try again.\n");
             }
